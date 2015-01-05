e73820599ef4d747e02f1b776c9de53b2a92ccc5
Daniel Cardenas <dan85.cardenas@gmail.com>
2015-01-05 10:13:21 -0500

    Add specs and code for editing todo items

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -10,7 +10,7 @@ class TodoItemsController < ApplicationController
 
   def create
   	@todo_list = TodoList.find(params[:todo_list_id])
-  	@todo_item = @todo_list.todo_items.new (todo_item_params)
+  	@todo_item = @todo_list.todo_items.new(todo_item_params)
   	if @todo_item.save	
   		flash[:success] = "Added todo list item."
   		redirect_to todo_list_todo_items_path
@@ -19,6 +19,28 @@ class TodoItemsController < ApplicationController
   		render action: :new
   	end
 
+  end
+
+  def edit
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+  
+  end
+
+  def update
+    @todo_list = TodoList.find(params[:todo_list_id])
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.update_attributes(todo_item_params)
+      flash[:success] = "Saved todo list item."
+      redirect_to todo_list_todo_items_path
+    else
+      flash[:error] = "That todo item could not be saved."
+      render action: :edit
+    end
+  end
+
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
   end
 
   private 
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,4 +1,6 @@
 class TodoItem < ActiveRecord::Base
   belongs_to :todo_list
 
+  validates :content, presence: true
+
 end
